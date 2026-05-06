a6b7a8b60c182cb55b2fe2477d12bc1ee2830260
Crocros GitHub Adventures 3D! <nilufaislam109@gmail.com>
2026-05-07 01:47:49 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/not-msn-tv-2/',
+  base: '/fotosintese-center/',
   plugins: [react(), tailwindcss()],
 })
